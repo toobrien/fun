8e008f5bcd21e0fb97b47304e58b556d3f388cbc
Taylor O'Brien <toobrien@amazon.com>
2025-08-18 23:26:34 -0700

    .

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,27 +10,62 @@ import (
 	"gonum.org/v1/gonum/stat/sampleuv"
 )
 
+const (
+	sample_size = 1000
+	easy        = 0.60
+	medium      = 0.55
+	hard        = 0.51
+)
+
+var difficulty = map[string]float64{
+	"easy":   0.60,
+	"medium": 0.55,
+	"hard":   0.51,
+}
+
+//var requests = map[string]bool{}
+
 func index(w http.ResponseWriter, r *http.Request) {
 
 	http.ServeFile(w, r, "./index.html")
 
 }
 
-func next_(w http.ResponseWriter, r *http.Request) {
+func next(w http.ResponseWriter, r *http.Request) {
 
-	b := distuv.Bernoulli{P: 0.5}
-	out := make([]float64, 1000)
+	dif := r.URL.Query().Get("difficulty")
+
+	if dif == "" {
+		fmt.Fprintf(w, "missing querystring parameter difficulty")
+		return
+	}
+
+	p_, ok := difficulty[dif]
+
+	if !ok {
+		fmt.Fprintf(w, "invalid difficulty: %v", dif)
+		return
+	}
+
+	b := distuv.Bernoulli{P: p_}
+	x := make([]float64, sample_size)
 	smpl := sampleuv.IIDer{Dist: b}
-	smpl.Sample(out)
+	smpl.Sample(x)
 
-	fmt.Fprintf(w, "%v", out)
+	res := struct {
+		Result []float64 `json:"res"`
+	}{Result: x}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(res)
 
 }
 
 func main() {
 
 	http.HandleFunc("/", index)
-	http.HandleFunc("/next_", next_)
+	http.HandleFunc("/next_", next)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
